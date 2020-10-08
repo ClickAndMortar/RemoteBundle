b33bd47dfbb7d775e1a820c4a7d202912e2e75a4
Simon CARRE <simoncarre54@gmail.com>
2020-10-08 16:27:08 +0200

    Update put remote files command for sftp

--- Command/PutRemoteFilesCommand.php
+++ Command/PutRemoteFilesCommand.php
@@ -2,11 +2,12 @@
 
 namespace ClickAndMortar\RemoteBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use phpseclib\Net\SFTP;
 
 /**
  * Put files to a remote server (FTP / SSH / ...)
@@ -14,7 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @author  Simon CARRE <simon.carre@clickandmortar.fr>
  * @package ClickAndMortar\Bundle\CatalogBundle\Command
  */
-class PutRemoteFilesCommand extends ContainerAwareCommand
+class PutRemoteFilesCommand extends Command
 {
     /**
      * Default SFTP connection port
@@ -37,8 +38,7 @@ class PutRemoteFilesCommand extends ContainerAwareCommand
      */
     protected function configure()
     {
-        $this->setName('candm:remote:put')
-             ->setDescription('Put files to a remote server (FTP / SSH / ...)')
+        $this->setDescription('Put files to a remote server (FTP / SSH / ...)')
              ->addArgument('server', InputArgument::REQUIRED, 'Server')
              ->addArgument('user', InputArgument::REQUIRED, 'User')
              ->addArgument('localFilePath', InputArgument::REQUIRED, 'Local file path: /tmp/my_file_*.txt')
@@ -94,6 +94,7 @@ class PutRemoteFilesCommand extends ContainerAwareCommand
      */
     protected function putRemoteFilesBySftp($server, $user, $port, $distantFilePath, $localFilePath, $password = null, $deleteAfterUpload = false)
     {
+        // Check for local files
         $localFilePaths = glob($localFilePath);
         if (!is_array($localFilePaths)) {
             $this->output->writeln('<error>No local files matching path.</error>');
@@ -101,63 +102,24 @@ class PutRemoteFilesCommand extends ContainerAwareCommand
             return;
         }
 
+        // Open connection
+        $sftpClient = new SFTP($server, $port);
+        if (!$sftpClient->login($user, $password)) {
+            $this->output->writeln(sprintf('<error>Can not open connection to server %s with user %s</error>', $server, $user));
+
+            return;
+        }
+
+        // Upload files
+        $isDistantDirectoryPath = substr($distantFilePath, -1) == DIRECTORY_SEPARATOR;
         foreach ($localFilePaths as $localFilePath) {
-            if (!file_exists($localFilePath)) {
-                $this->output->writeln(sprintf('<error>Invalid local file path : %s</error>', $localFilePath));
-
-                return;
+            if ($isDistantDirectoryPath) {
+                $distantFilePath = sprintf('%s%s', $distantFilePath, basename($localFilePath));
             }
-
-            // Init SFTP connection
-            $connection = ssh2_connect($server, $port);
-            ssh2_auth_password($connection, $user, $password);
-            $sftpConnection = ssh2_sftp($connection);
-            $sftpBasePath   = intval($sftpConnection);
-
-            // Open distant file
-            if (substr($distantFilePath, -1) == DIRECTORY_SEPARATOR) {
-                $localFilename   = basename($localFilePath);
-                $sftpDistantPath = sprintf(
-                    'ssh2.sftp://%s%s%s',
-                    $sftpBasePath,
-                    $distantFilePath,
-                    $localFilename
-                );
-            } else {
-                $sftpDistantPath = sprintf(
-                    'ssh2.sftp://%s%s',
-                    $sftpBasePath,
-                    $distantFilePath
-                );
+            $this->output->writeln(sprintf('<info>Update local file %s...</info>', $localFilePath));
+            if (!$sftpClient->put($distantFilePath, $localFilePath, SFTP::SOURCE_LOCAL_FILE)) {
+                $this->output->writeln(sprintf('<error>Can not upload local file %s</error>', $localFilePath));
             }
-
-            $distantFile = fopen($sftpDistantPath, 'w');
-            if ($distantFile === false) {
-                $this->output->writeln('<error>Can not write distant file.</error>');
-
-                return;
-            }
-
-            // And local file
-            $localFile = fopen($localFilePath, 'r');
-            if ($localFile === false) {
-                $this->output->writeln('<error>Can not read local file.</error>');
-
-                return;
-            }
-
-            // And copy
-            $writtenBytes = stream_copy_to_stream($localFile, $distantFile);
-            if ($writtenBytes === false) {
-                $this->output->writeln('<error>Can not copy file.</error>');
-
-                return;
-            }
-
-            fclose($distantFile);
-            fclose($localFile);
-
-            // Delete local file if necessary
             if ($deleteAfterUpload) {
                 unlink($localFilePath);
             }
